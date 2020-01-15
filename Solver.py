5a7addd2dc3dc68103f37713ee43d4ec3d8c9f16
Wenqi-Zhu <59636792+Wenqi-Zhu@users.noreply.github.com>
2020-01-15 16:02:39 +0900

    Update Solver.py

--- Solver.py
+++ Solver.py
@@ -1,46 +1,9 @@
 import numpy as np
-import numba
-from numba import jit
-from .ClassDef import Circuit
 from math import pi as π
-from .ErrorDefine import *
 import numpy as np
 
-@jit(nopython=True)
-def Simulator(
-    Circuit:Circuit,
-    θ_Interval:tuple,
-    Initial_State:np.matrix,
-    Step=100,
-    Output_Record=True):
-    '''
-    Run simulation for this circuit.
-    '''
-    Record=np.hstack((0.0,Initial_State))
-    Start_θ, Record_θ, End_θ = 0.0, θ_Interval[0], θ_Interval[1] 
-    if Record_θ > End_θ:
-        raise CircuitParameterError("Record_θ cannot be larger than End_θ!")
-    Period = End_θ / (2.0 * π)
-    TotalStep = int(Period * Step)
-    θSpace, StepSize = np.linspace(Start_θ,End_θ,num=TotalStep,retstep=True)
-    States=[Initial_State]*4
-    
-    for θ in θSpace:
-        Mat_A=Circuit.MatA(θ)
-        Mat_B=Circuit.MatB(θ,States[0])
-        States = [BDF_4(States,Mat_A,Mat_B,StepSize)] + States
-        if Output_Record and θ > Record_θ:
-            Line=np.hstack(([θ],States[0]))
-            Record.append(Line)
-        States.pop
-
-    if not Output_Record:
-        Record+=[θ]+States[0]
-    #Adjust the order of output record
-    return Record
-@jit(nopython=True)
 def BDF_4(
-        States:list,
+        States:np.array,
         MatA:np.matrix,
         MatB:np.matrix,
         h:float
@@ -55,9 +18,8 @@ def BDF_4(
             4.h (Step size per one step)
         '''
         #Order = np.size(MatA,0)
-        Order = 7
-        Diag=-25.0/12.0*np.identity(Order)
-        MatB = MatB * h + Diag
+        Order = 8
+        MatB = MatB * h - 25.0/12.0*np.identity(Order)
         MatA = -h*MatA-States[0]*4.0+States[1]*3.0-States[2]*(4.0/3.0)+States[3]*0.25
         MatB_Inv = np.linalg.inv(MatB)
         Result = np.dot(MatB_Inv,MatA)
