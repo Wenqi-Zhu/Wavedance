02ea393bfa83c0fd022197f6443a3ec710785c07
Wenqi-Zhu <59636792+Wenqi-Zhu@users.noreply.github.com>
2020-01-16 15:30:42 +0900

    [ver. 0.0.2]Some new features added
    1.Designer. 
    Now you can use Designer to design the parameters of the circuit, (by Newton's method).
    
    2.Oscilloscope now draws concrete voltage and current waveforms.
    
    3.Some minor changes

--- ClassDef.py
+++ ClassDef.py
@@ -4,48 +4,54 @@ from math import pi
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+
+
 class Circuit(object):
-    def Para_Check(self,Para_Dict):
+    Vi = 1.0,
+    RL = 1.0,
+    fs = 1e6,
+
+    def Para_Check(self, Para_Dict):
         '''
         Check if parameters are enough to create a circuit.
         '''
         for Para in self.Para_List:
             if Para not in Para_Dict:
                 raise CircuitParameterError("Parameters are not enough. Check your input.")
-    
+
     def Run(
-        self,
-        θ_Interval:tuple,
-        Initial_State:np.matrix,
-        Step=200,
-        Output_Record=True,
-        Silent=False):
+            self,
+            θ_Interval: tuple,
+            Initial_State: np.matrix,
+            Step=200,
+            Output_Record=True,
+            Silent=False):
         '''
         Run simulation for this circuit.
         '''
-        Record=pd.DataFrame()
-        Start_θ, Record_θ, End_θ = 0.0, θ_Interval[0], θ_Interval[1] 
-        if Record_θ > End_θ :
+        Record = pd.DataFrame()
+        Start_θ, Record_θ, End_θ = 0.0, θ_Interval[0], θ_Interval[1]
+        if Record_θ > End_θ:
             raise CircuitParameterError("Record_θ cannot be larger than End_θ!😯")
         Period = End_θ / (2.0 * pi)
         TotalStep = int(Period * Step)
-        θSpace, StepSize = np.linspace(Start_θ,End_θ,num=TotalStep,retstep=True)
+        θSpace, StepSize = np.linspace(Start_θ, End_θ, num=TotalStep, retstep=True)
         if not Silent:
             θSpace = tqdm(θSpace)
-        States=[Initial_State]*4
-        
+        States = [Initial_State] * 4
+
         for θ in θSpace:
-            Mat_A=self.MatA(θ)
-            Mat_B=self.MatB(θ,States[0])
-            States = [BDF_4(States,Mat_A,Mat_B,StepSize)] + States
+            Mat_A = self.MatA(θ)
+            Mat_B = self.MatB(θ, States[0])
+            States = [BDF_4(self.order,States, Mat_A, Mat_B, StepSize)] + States
             if Output_Record and θ > Record_θ:
-                Record=self.CreateDataFrame(θ,States[0],Record)
+                Record = self.CreateDataFrame(θ, States[0], Record)
             States.pop
 
         if not Output_Record:
-            Record=self.CreateDataFrame(θ,States[0],Record)
-        #Adjust the order of output record
-        Record=Record[self.Output_List]
+            Record = self.CreateDataFrame(θ, States[0], Record)
+        # Adjust the order of output record
+        Record = Record[self.Output_List]
         return Record
 
 
--- Test.py
+++ Test.py
@@ -1,40 +1,42 @@
-
 import numpy as np
 from math import pi as π
 from CircuitLibrary.ClassEDWPT import ClassEDWPT
 from Oscilloscope import Oscilloscope
 from Designer import Designer
-#Create a new Class-E2 DC DC Converter
-#and name it "My_Circuit"
+
+
+# Create a new Class-E2 DC DC Converter
+# and name it "My_Circuit"
 My_Circuit = ClassEDWPT(
-                        #Parameters:
-                        A=0.9258,   B=0.6461,  Q=0.25,    H=20.0,     
-                        N=1.0,     k=0.20,    S=0.02,   J=5.0,
-                        Ds=0.5,    U=1.0,
-                        #Parasitic resistances：      
-                        rL1=0.002,  rL2=0.002,   rS=0.002, rD=0.002  
-                        )
+    # Basic Quantities:
+    Vi=5.0,  RL=5.0,  fs=1e6,
+    # Parameters:
+    A=0.9258, B=0.6461, Q=0.25, H=20.0,
+    N=1.0,    k=0.20,   S=0.02, J=5.0,
+    Ds=0.5,   U=1.0,
+    # Parasitic resistances：
+    rL1=0.002, rL2=0.002, rS=0.002, rD=0.002
+)
 
-#Initial state (zero 8x1 matrix)
-Initial_State=np.zeros(8)
+# Initial state (zero 8x1 matrix)
+Initial_State = np.zeros(8)
 
-#Time to start record and end
-#(θ_StartRecord, θ_End)
-θInterval=(200.*π,204.*π) 
+# Time to start record and end
+# (θ_StartRecord, θ_End)
+θInterval = (200. * π, 202. * π)
 
-#Target Output Voltage
-TargetVo=2.7
+# Target Output Voltage
+TargetVo = 15.0
 
-#Run Newton's Method
-My_Circuit2 = Designer(My_Circuit,TargetVo)
+# Run Newton's Method
+My_Circuit2 = Designer(My_Circuit, TargetVo)
 
-#Run simulation, let's rock!
-#The waveform will be saved in dataframe "Record"
-Record=My_Circuit2.Run(θInterval,Initial_State)
+# Run simulation, let's rock!
+# The waveform will be saved in dataframe "Record"
+Record = My_Circuit2.Run(θInterval, Initial_State)
 
-#Now, see the waveform by the method "Oscilloscope"
-Graph=Oscilloscope(Record,['iC','vS','i1','i2','vD1','vO'])
+# Now, see the waveform by the method "Oscilloscope"
+Graph = Oscilloscope(Record, ['iC', 'vS', 'i1', 'i2', 'vD1', 'vO'])
 
-#You can also output "Record" to csv file
-#Record.to_csv('Data//Record.csv',encoding='utf_8_sig')
-
+# You can also output "Record" to csv file
+# Record.to_csv('Data//Record.csv',encoding='utf_8_sig')
